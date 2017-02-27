9d5835101ae50e02d37637f232060ced26a9ba2e
arpieb <rob@octobang.com>
2017-02-27 16:29:29 -0500

    Added starter deps, updated mix.exs with Hex package info

--- mix.exs
+++ mix.exs
@@ -2,12 +2,27 @@ defmodule CoreNLP.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :corenlp,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :corenlp,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: description(),
+      package: package(),
+
+      # Docs.
+      name: "CoreNLP",
+      source_url: "https://github.com/arpieb/corenlp",
+      homepage_url: "https://github.com/arpieb/corenlp",
+      docs: [
+        main: "readme",
+        extras: [
+          "README.md",
+        ]
+      ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,6 +43,33 @@ defmodule CoreNLP.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.15.0", only: :dev},
+      {:httpoison, "~> 0.11.0"},
+      {:poison, "~> 3.1"},
+    ]
+  end
+
+  defp description do
+    """
+    This package provides a client interface to a Stanford CoreNLP Server.
+    """
+  end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "mix.exs",
+        "README*",
+        "LICENSE*",
+      ],
+      maintainers: ["Robert Bates"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/arpieb/corenlp",
+        "CoreNLP" => "http://stanfordnlp.github.io/CoreNLP/index.html",
+      },
+    ]
   end
 end
